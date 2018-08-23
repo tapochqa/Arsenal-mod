aaa44820b655d42ddc724efd6daf88ca1e4ac034
Maxem <choochooh55@gmail.com>
2018-08-23 23:30:35 +0300

    Продолжение фикса имени

--- Items/Woodgun.cs
+++ Items/Woodgun.cs
@@ -2,7 +2,7 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-namespace SimpleMod.Items
+namespace ArsenalMod.Items
 {
 	public class Woodgun : ModItem
 	{
