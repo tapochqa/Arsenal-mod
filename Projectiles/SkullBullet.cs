1db0f5db33f3b9d7ad5790c9560fc85508b453e6
Daniil <nabiriy.d@gmail.com>
2018-08-25 22:42:17 +0300

    физика гранат (не тестил)

---
+++ Projectiles/SkullBullet.cs
@@ -0,0 +1,179 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ArsenalMod.Projectiles
+{
+	public class SkullBullet : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Skull bullet");     //The English name of the projectile
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;    //The length of old position to be recorded
+			ProjectileID.Sets.TrailingMode[projectile.type] = 0;        //The recording mode
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 14;               //The width of projectile hitbox
+			projectile.height = 16;              //The height of projectile hitbox
+			projectile.aiStyle = 1;             //The ai style of the projectile, please reference the source code of Terraria
+			projectile.friendly = true;         //Can the projectile deal damage to enemies?
+			projectile.hostile = false;         //Can the projectile deal damage to the player?
+			projectile.ranged = true;           //Is the projectile shoot by a ranged weapon?
+			projectile.penetrate = 5;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
+			projectile.timeLeft = 600;          //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+			projectile.alpha = 255;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in)
+			projectile.light = 0.5f;            //How much light emit around the projectile
+			projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
+			projectile.tileCollide = true;          //Can the projectile collide with tiles?
+			projectile.extraUpdates = 1;            //Set to above 0 if you want the projectile to update multiple time in a frame
+			aiType = ProjectileID.Grenade;           //Act exactly like default Bullet
+		}
+		public override void AI()
+		{
+			if (projectile.owner == Main.myPlayer && projectile.timeLeft <= 3)
+			{
+				projectile.tileCollide = false;
+				// Set to transparent. This projectile technically lives as  transparent for about 3 frames
+				projectile.alpha = 255;
+				// change the hitbox size, centered about the original projectile center. This makes the projectile damage enemies during the explosion.
+				projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+				projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+				projectile.width = 250;
+				projectile.height = 250;
+				projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+				projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+				projectile.damage = 250;
+				projectile.knockBack = 10f;
+			}
+
+			projectile.ai[0] += 1f;
+			if (projectile.ai[0] > 5f)
+			{
+				projectile.ai[0] = 10f;
+				// Roll speed dampening.
+				if (projectile.velocity.Y == 0f && projectile.velocity.X != 0f)
+				{
+					projectile.velocity.X = projectile.velocity.X * 0.97f;
+					//if (projectile.type == 29 || projectile.type == 470 || projectile.type == 637)
+					{
+						projectile.velocity.X = projectile.velocity.X * 0.99f;
+					}
+					if ((double)projectile.velocity.X > -0.01 && (double)projectile.velocity.X < 0.01)
+					{
+						projectile.velocity.X = 0f;
+						projectile.netUpdate = true;
+					}
+				}
+				projectile.velocity.Y = projectile.velocity.Y + 0.2f;
+			}
+			// Rotation increased by velocity.X 
+			projectile.rotation += projectile.velocity.X * 0.1f;
+			return;
+		}
+
+			public override void Kill(int timeLeft)
+		{
+
+			// Play explosion sound
+			Main.PlaySound(SoundID.Item15, projectile.position);
+			// Smoke Dust spawn
+			for (int i = 0; i < 50; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 31, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 1.4f;
+			}
+			// Fire Dust spawn
+			for (int i = 0; i < 80; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 3f);
+				Main.dust[dustIndex].noGravity = true;
+				Main.dust[dustIndex].velocity *= 5f;
+				dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 6, 0f, 0f, 100, default(Color), 2f);
+				Main.dust[dustIndex].velocity *= 3f;
+			}
+			// Large Smoke Gore spawn
+			for (int g = 0; g < 2; g++)
+			{
+				int goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X + 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y + 1.5f;
+				goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X - 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y + 1.5f;
+				goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X + 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y - 1.5f;
+				goreIndex = Gore.NewGore(new Vector2(projectile.position.X + (float)(projectile.width / 2) - 24f, projectile.position.Y + (float)(projectile.height / 2) - 24f), default(Vector2), Main.rand.Next(61, 64), 1f);
+				Main.gore[goreIndex].scale = 1.5f;
+				Main.gore[goreIndex].velocity.X = Main.gore[goreIndex].velocity.X - 1.5f;
+				Main.gore[goreIndex].velocity.Y = Main.gore[goreIndex].velocity.Y - 1.5f;
+			}
+			// reset size to normal width and height.
+			projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+
+			// TODO, tmodloader helper method
+			{
+				int explosionRadius = 3;
+				//if (projectile.type == 29 || projectile.type == 470 || projectile.type == 637)
+				{
+					explosionRadius = 7;
+				}
+				int minTileX = (int)(projectile.position.X / 16f - (float)explosionRadius);
+				int maxTileX = (int)(projectile.position.X / 16f + (float)explosionRadius);
+				int minTileY = (int)(projectile.position.Y / 16f - (float)explosionRadius);
+				int maxTileY = (int)(projectile.position.Y / 16f + (float)explosionRadius);
+				if (minTileX < 0)
+				{
+					minTileX = 0;
+				}
+				if (maxTileX > Main.maxTilesX)
+				{
+					maxTileX = Main.maxTilesX;
+				}
+				if (minTileY < 0)
+				{
+					minTileY = 0;
+				}
+				if (maxTileY > Main.maxTilesY)
+				{
+					maxTileY = Main.maxTilesY;
+				}
+				bool canKillWalls = false;
+				bool canKillTile = false;
+				
+			}
+		}
+
+
+
+
+
+
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			//Redraw the projectile with the color not influenced by light
+			Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+			for (int k = 0; k < projectile.oldPos.Length; k++)
+			{
+				Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+				Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+				spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+			}
+			return true;
+		}
+	}
+}
