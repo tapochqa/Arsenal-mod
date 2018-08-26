ecf53ea2495cec169f8c579dbb6e1c36f8392d66
Daniil <nabiriy.d@gmail.com>
2018-08-26 14:03:50 +0300

    back

--- Items/Ammo/SkullBullet.cs
+++ Items/Ammo/SkullBullet.cs
@@ -9,6 +9,7 @@ namespace ArsenalMod.Items.Ammo
 		public override void SetStaticDefaults()
 		{
 			Tooltip.SetDefault("Corpse");
+			Tooltip.SetDefault("Warning! Still bites!");
 		}
 
 		public override void SetDefaults()
